e11362457ff88f02d303f4515f0d86d227ae2af6
Madan Kumar <kumarmadan.j@gmail.com>
2020-12-19 20:37:37 +0530

    Update stack implementation

--- javascript/lib/Stack.js
+++ javascript/lib/Stack.js
@@ -1,41 +1,40 @@
 class Stack {
-  capacity = 10;
-  top = -1;
-  arr = [];
+  constructor(size = 10) {
+    this.capacity = size;
+    this.arr = [];
+    this.top = -1;
+    this.size = 0;
+  }
 
-  constructor(size) {
-    if (typeof size === "number") {
-      this.capacity = size;
-    }
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  isFull() {
+    return this.size === this.capacity;
   }
 
   push(value) {
-    if (this.top === this.capacity - 1) {
+    if (this.isFull()) {
       throw new Error("Stack overflow");
     }
     this.top++;
+    this.size++;
     this.arr.push(value);
   }
 
   pop() {
-    if (this.top === -1) {
+    if (this.isEmpty()) {
       throw new Error("Stack underflow");
     }
 
     this.top--;
+    this.size--;
     return this.arr.pop();
   }
 
   peek() {
     return this.arr[this.top];
-  }
-
-  isEmpty() {
-    return this.top === -1;
-  }
-
-  get size() {
-    return this.arr.length;
   }
 }
 
--- javascript/test/stack.test.js
+++ javascript/test/stack.test.js
@@ -64,4 +64,15 @@ describe("Test stack data structure", () => {
 
     expect(stack.isEmpty()).toBeTruthy();
   });
+
+  test('should return stack is full',()=>{
+    const stack = new Stack(5);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+
+    expect(stack.isFull()).toBeTruthy();
+  });
 });
